f78cda0e66f12357249093277b1b63d3b05d06c5
Alexandre Calaça <alexcalaca@gmail.com>
2025-03-14 21:17:34 -0300

    add 'all navigators' project

---
+++ class06/all-navigators-class06/screens/SettingsScreen.js
@@ -0,0 +1,11 @@
+// screens/SettingsScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24 }}>Settings Screen</Text>
+    </View>
+  );
+}
